af345b96a40b315a501b66309cf9afc2857c74bd
Anton <B_atonyo_S@bk.ru>
2023-05-26 21:01:12 +0300

    Task 3 completed

---
+++ 3/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Напишите программу, которая принимает на вход число (N)
+//  и выдаёт таблицу квадратов чисел от 1 до N.
+
+// 5 -> 1, 4, 9, 16, 25.
+
+// 2 -> 1,4
+Console.Clear();
+int count = 0, index =1;
+while (count<1)
+{
+System.Console.WriteLine("Введите положительное число");
+count = int.Parse(Console.ReadLine());
+}
+Console.Write($"{count} ->");
+while (index<=count)
+{
+    Console.Write($" {Math.Pow(index,2)} ");
+    index++;
+} 
+System.Console.WriteLine();
+
